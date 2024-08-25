d63570de20f56f5e35415f9eafb0d0a390acb1e8
Duvan <duvanriano2626@gmail.com>
2024-08-25 14:27:40 -0500

    Uso del servicio en mi componente

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { UsersServiceService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-user-info-page',
   templateUrl: './user-info-page.component.html',
   styleUrl: './user-info-page.component.css'
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit{
+
+  private userServices = inject(UsersServiceService);
+  public userId = signal(1);
+  public currentUser = signal<User|undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser(this.userId())
+  }
+
+
+
+  public loadUser = (id:number)  =>{
+    if (id <=0 ) {
+      return
+    }
+    this.userId.set(id)
+    this.currentUser.set(undefined);
+    this.userServices.getUserById(id)
+      .subscribe( user => {
+        // console.log({user});
+        this.currentUser.set(user);
+      })
+
+
+  }
+
+
 
 }
