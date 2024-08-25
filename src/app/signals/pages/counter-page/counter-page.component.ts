1dfe5b64d1c56e926134206a25fd89e7d6c5c4e2
Duvan <duvanriano2626@gmail.com>
2024-08-25 13:22:47 -0500

    Creacion de señales para modificar, resetear y actualizar data muy importante

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-counter-page',
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrl: './counter-page.component.css'
 })
 export class CounterPageComponent {
+
+  public counter = signal(10);
+  public squareCounter = computed(() => this.counter() * this.counter()); //Propiedad de solo lectura
+
+
+
+
+  public increaseBy = (value:number) => {
+    this.counter.update(current => current + value)
+  }
 
 }
