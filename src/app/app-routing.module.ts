d7a6bf2d57e0d0fe003d37eff3956581c3a2938e
Duvan <duvanriano2626@gmail.com>
2024-08-25 12:39:54 -0500

    Inicio del proyecto creacion de la estructura del proyecto modulos, layouts, interfaces, componentes, servicios, pages y creacion de las rutas de mi proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'signals',
+    loadChildren: () => import('./signals/signals.module').then(m => m.SignalsModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
