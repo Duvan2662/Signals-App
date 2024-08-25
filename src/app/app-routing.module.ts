c0d32a4e6b8d3bbc9df491f5d3c561184e4fec14
Duvan <duvanriano2626@gmail.com>
2024-08-25 13:46:16 -0500

    Creacion del template donde se va a mostrar la informacion de un usuario, creacion del servicio donde vamos a recibir la peticion http, creacion de la interface de nuestra peticion http importancion importante en el AppRoutinModule para que funcione lo de la peticion http

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,10 @@ const routes: Routes = [
   {
     path:'signals',
     loadChildren: () => import('./signals/signals.module').then(m => m.SignalsModule)
+  },
+  {
+    path:'**',
+    redirectTo:'signals'
   }
 ];
 
